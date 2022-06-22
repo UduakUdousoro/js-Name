029a8742c30c4372b3e5e32c35cd5d90dfb05e43
Ud2007 <uduakudousoro16@gmail.com>
2022-06-22 23:10:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("A little information about me.")
+
+//I used the backticks to wrap the text, because of the quotations in the middle of the string
+let z = `Name: "Uduak Udousoro"\nHeight: "5'6"\nCountry: "Nigeria"`
+
+alert(z) 
